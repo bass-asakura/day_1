eafacc2bb317af082185db5a18c2c4ee637a6fb7
bass <msileha@mail.ru>
2025-03-24 15:24:28 +0300

    Последние штрихи

--- Program.cs
+++ Program.cs
@@ -34,19 +34,28 @@ class Program
     {
         Operation.Start();
 
-        Console.WriteLine("Введите имя");
-        string name = Console.ReadLine() ?? "";
-        Console.WriteLine("Введите возраст");
-        int age = Convert.ToInt32(Console.ReadLine());
-
-        Func<string, bool> nameCheck = nameSepar;
-
-        if (nameCheck(name))
+        try
         {
-            Console.WriteLine("User is reg");
-            EventHelper.OnUserCreated(name, age);
+            Console.WriteLine("Введите имя");
+            string name = Console.ReadLine() ?? "";
+            Console.WriteLine("Введите возраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+        
+
+            Func<string, bool> nameCheck = nameLen;
+
+            if (nameCheck(name))
+            {
+                Console.WriteLine("User is reg");
+                EventHelper.OnUserCreated(name, age);
+            }
+        }
+
+        catch (FormatException)
+        {
+            Console.WriteLine("Неверный ввод");
         }
     }
 
-    public static bool nameSepar(string name) {return name != null;}
+    public static bool nameLen(string name) {return name.Length != 0;}
 }
