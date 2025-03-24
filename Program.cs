aece9cc8d16d6a55a6178bae185f43218b481a9d
bass <msileha@mail.ru>
2025-03-24 15:23:06 +0300

    Сделаны все функции по ТЗ

--- Program.cs
+++ Program.cs
@@ -1,9 +1,52 @@
 ﻿namespace day_1;
 
+class EventHelper
+{
+    public static event Action<string, int> UserCreated = default!;
+
+    public static void OnUserCreated(string name, int age)
+    {
+        UserCreated?.Invoke(name, age);
+    }
+}
+
+class Operation
+{
+    public static void Start()
+    {
+        EventHelper.UserCreated += createUser;
+    }
+    
+    public static void createUser(string name, int age)
+    {
+        Console.WriteLine($"Привет, {name}");
+        
+        if (age >= 18)
+        {
+            File.AppendAllText(@"C:\vs code\task_1\userlist", name + " " + age + "\n");
+        }
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Operation.Start();
+
+        Console.WriteLine("Введите имя");
+        string name = Console.ReadLine() ?? "";
+        Console.WriteLine("Введите возраст");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        Func<string, bool> nameCheck = nameSepar;
+
+        if (nameCheck(name))
+        {
+            Console.WriteLine("User is reg");
+            EventHelper.OnUserCreated(name, age);
+        }
     }
+
+    public static bool nameSepar(string name) {return name != null;}
 }
